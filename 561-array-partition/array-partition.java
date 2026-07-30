0b13e24e93a85049c4dcceea4e6448d7915029f0
Shreyash-0205 <shreyashvanjari69@gmail.com>
2026-07-30 17:23:55 +0530

    Time: 17 ms (81.26%) | Memory: 49.7 MB (35.79%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -4,11 +4,10 @@ class Solution {
         Arrays.sort(nums);
 
         int sum = 0;
-
-        for (int i = 0; i < nums.length; i += 2) {
-            sum += nums[i];
+        
+        for(int i = 0 ;i<nums.length;i+=2){
+            sum+=nums[i];
         }
-
         return sum;
     }
 }
